bb124a59374c85975af3c61fed2887f2baa8fb1f
Nándor Dudás <nandor.dudas@gmail.com>
2023-09-22 20:27:31 +0000

    fix: add missing override keyword

--- packages/server/src/handlers/a-handler.ts
+++ packages/server/src/handlers/a-handler.ts
@@ -4,7 +4,7 @@ import { BaseHandler } from './base-handler'
 export class AHandler<T extends string, P = any> extends BaseHandler<T, P> {
   public readonly type = 'A'
 
-  public handle(data: Message<T, P>): void {
+  public override handle(data: Message<T, P>): void {
     this.logger('AHandler %o', data)
   }
 }
